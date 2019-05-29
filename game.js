0a637971d4f36a6af454acfd0984363d7a526e32
padmavathi5 <nunnapadmavathi2000@gmail.com>
2019-05-29 15:16:43 +0530

    changes for gam.html

---
+++ game.js
@@ -0,0 +1,122 @@
+$(document).ready(function () {
+var count=0;
+function clickablediv1() {
+	if(count%2==0){
+	document.getElementById("but1").style.backgroundColor = "red";		
+	}else{
+		document.getElementById("but1").style.backgroundColor = "green";
+	}
+  	count++;
+}
+function clickablediv2() {
+	if(count%2==0){
+	document.getElementById("but2").style.backgroundColor = "red";		
+	}else{
+		document.getElementById("but2").style.backgroundColor = "green";
+	}
+  	count++;
+}function clickablediv3() {
+	if(count%2===0){
+	document.getElementById("but3").style.backgroundColor = "red";		
+	}else{
+		document.getElementById("but3").style.backgroundColor = "green";
+	}
+  	count++;
+}function clickablediv4() {
+	if(count%2===0){
+	document.getElementById("but4").style.backgroundColor = "red";		
+	}else{
+		document.getElementById("but4").style.backgroundColor = "green";
+	}
+  	count++;
+}function clickablediv5() {
+	if(count%2===0){
+	document.getElementById("but5").style.backgroundColor = "red";		
+	}else{
+		document.getElementById("but5").style.backgroundColor = "green";
+	}
+  	count++;
+}function clickablediv6() {
+	if(count%2===0){
+	document.getElementById("but6").style.backgroundColor = "red";		
+	}else{
+		document.getElementById("but6").style.backgroundColor = "green";
+	}
+  	count++;
+}function clickablediv7() {
+	if(count%2===0){{
+	document.getElementById("but7").style.backgroundColor = "red";		
+	}else{
+		document.getElementById("but7").style.backgroundColor = "green";
+	}
+  	count++;
+}function clickablediv8() {
+	if(count%2===0){
+	document.getElementById("but8").style.backgroundColor = "red";		
+	}else{
+		document.getElementById("but8").style.backgroundColor = "green";
+	}
+  	count++;
+}function clickablediv9() {
+	if(count%2===0){
+	document.getElementById("but9").style.backgroundColor = "red";		
+	}else{
+		document.getElementById("but9").style.backgroundColor = "green";
+	}
+  	count++;
+}function clickablediv10() {
+	if(count%2===0){
+	document.getElementById("but10").style.backgroundColor = "red";		
+	}else{
+		document.getElementById("but10").style.backgroundColor = "green";
+	}
+  	count++;
+}function clickablediv11() {
+	if(count%2===0){
+	document.getElementById("but11").style.backgroundColor = "red";		
+	}else{
+		document.getElementById("but11").style.backgroundColor = "green";
+	}
+  	count++;
+}function clickablediv12() {
+	if(count%2===0){
+	document.getElementById("but12").style.backgroundColor = "red";		
+	}else{
+		document.getElementById("but12").style.backgroundColor = "green";
+	}
+  	count++;
+}function clickablediv13() {
+	if(count%2===0){
+	document.getElementById("but13").style.backgroundColor = "red";		
+	}else{
+		document.getElementById("but13").style.backgroundColor = "green";
+	}
+  	count++;
+}function clickablediv14() {
+	if(count%2===0){
+	document.getElementById("but14").style.backgroundColor = "red";		
+	}else{
+		document.getElementById("but14").style.backgroundColor = "green";
+	}
+  	count++;
+}function clickablediv15() {
+	if(count%2===0){
+	document.getElementById("but15").style.backgroundColor = "red";		
+	}else{
+		document.getElementById("but15").style.backgroundColor = "green";
+	}
+  	count++;
+}function clickablediv16() {
+	if(count%2===0){
+	document.getElementById("but16").style.backgroundColor = "red";		
+	}else{
+		document.getElementById("but16").style.backgroundColor = "green";
+	}
+  	count++;
+}
+}
+
+
+
+
+
